28d301aee7c9cd7ea32cb14cce5572a03367c918
unknown <kodms08@naver.com>
2025-03-26 20:39:33 +0900

    init: 프로젝트 생성

---
+++ upbit-service/src/main/java/com/example/upbitservice/UpbitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.upbitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitServiceApplication.class, args);
+	}
+
+}
---
+++ upbit-service/src/test/java/com/example/upbitservice/UpbitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upbitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
