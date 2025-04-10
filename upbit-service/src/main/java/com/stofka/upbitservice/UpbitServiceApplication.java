bcd932b7270acee8e678b95973d675a6d2042116
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-04-10 14:18:22 +0900

    feat: 스케줄러 설정
    @EnavleSheduling 추가

--- upbit-service/src/main/java/com/stofka/upbitservice/UpbitServiceApplication.java
+++ upbit-service/src/main/java/com/stofka/upbitservice/UpbitServiceApplication.java
@@ -2,8 +2,10 @@ package com.stofka.upbitservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UpbitServiceApplication {
 
 	public static void main(String[] args) {
