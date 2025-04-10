206aada6b2b8991ef968dcf3a29fd07e2be4a0f9
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-04-10 14:13:33 +0900

    feat: 시간별 Candle Entity 생성

---
+++ upbit-service/src/main/java/com/stofka/upbitservice/candle/ThirtyMinuteCandles.java
@@ -0,0 +1,45 @@
+package com.stofka.upbitservice.candle;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "thirty_minute_candles", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"code", "candle_time"})
+})
+public class ThirtyMinuteCandles implements CandleBase{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    @Column(name = "candle_time")
+    private LocalDateTime candleTime;
+
+    @Column(name = "opening_price")
+    private BigDecimal openingPrice;
+
+    @Column(name = "high_price")
+    private BigDecimal highPrice;
+
+    @Column(name = "low_price")
+    private BigDecimal lowPrice;
+
+    @Column(name = "trade_price")
+    private BigDecimal tradePrice;
+
+    @Column(name = "candle_acc_trade_volume")
+    private BigDecimal candleAccTradeVolume;
+
+    @Column(name = "candle_acc_trade_price")
+    private BigDecimal candleAccTradePrice;
+}
